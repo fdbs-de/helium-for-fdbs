77db4f89ff2bfb6ad0da6ad3c41dce6c3b79ab17
Maurice Freuwört <maurice.freuwoert@gmail.com>
2023-03-22 16:30:55 +0100

    + Monaco Code Editor + Code Editor Vue component

--- resources/js/Components/Form/CodeEditor.vue
+++ resources/js/Components/Form/CodeEditor.vue
@@ -1,13 +1,70 @@
 <template>
-    <div class="wrapper">
-    </div>
+    <div class="monaco-code-editor-wrapper" id="monaco-editor"></div>
 </template>
 
 <script setup>
-    defineProps({
-        modelValue: String,
+    import { onMounted, onBeforeUnmount, watch, defineEmits } from 'vue'
+    import * as monaco from 'monaco-editor'
+
+
+
+    const emits = defineEmits(['update:modelValue'])
+    const props = defineProps({
+        modelValue: {
+            type: String,
+            default: ''
+        },
+        language: {
+            type: String,
+            default: 'html'
+        },
+        theme: {
+            type: String,
+            default: 'vs-dark'
+        },
+    })
+
+    let editor = null
+
+
+
+    onMounted(() => {
+        editor = monaco.editor.create(document.getElementById('monaco-editor'), {
+            value: props.modelValue ?? '',
+            language: props.language,
+            theme: props.theme,
+            automaticLayout: true,
+            minimap: {
+                enabled: true,
+            },
+        })
+
+        editor.onDidChangeModelContent((event) => {
+            emits('update:modelValue', editor.getValue())
+        })
+    })
+
+    onBeforeUnmount(() => {
+        editor.dispose()
+    })
+
+
+
+    watch(() => props.modelValue, (value) => {
+        if (editor.getValue() !== value) editor.setValue(value)
+    })
+
+    watch(() => props.language, (value) => {
+        monaco.editor.setModelLanguage(editor.getModel(), value)
+    })
+
+    watch(() => props.theme, (value) => {
+        monaco.editor.setTheme(value)
     })
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
+.monaco-code-editor-wrapper
+    height: 400px
+    width: 100%
 </style>
