7459295d8654817c37e3e7af2fa6ff16fde22276
Morris <maurice.freuwoert@gmail.com>
2023-05-17 11:42:57 +0200

    noch ein unendlich komplexe Aufgabe bewältigt

--- resources/js/Components/Page/Footer.vue
+++ resources/js/Components/Page/Footer.vue
@@ -69,12 +69,11 @@
                 </div>
                 <div class="spacer"></div>
                 <div class="social-wrapper">
-                    <!-- <a href="https://g.page/r/CWfKbNs4inYXEAg/review" target="_blank">Bewerten Sie uns auf Google</a> -->
-                    <!-- <span>•</span> -->
-                    <a href="https://www.ebay-kleinanzeigen.de/pro/FDBS" target="_blank" rel="opener" title="Ebay">Ebay</a>
-                    <span>•</span>
                     <a href="https://job38.de/arbeitgeber/fdbs-69115" target="_blank" rel="opener" title="job38">job38</a>
                     <span>•</span>
+                    <a href="https://www.kleinanzeigen.de/pro/FDBS" target="_blank" rel="opener" title="Kleinanzeigen">
+                        <img src="/images/assets/social-icons/icon_kleinanzeigen.svg" alt="Kleinanzeigen">
+                    </a>
                     <a href="https://g.page/r/CWfKbNs4inYXEAg/review" target="_blank" rel="opener" title="Bewerten Sie uns auf Google">
                         <img src="/images/assets/social-icons/icon_google.svg" alt="Bewerten Sie uns auf Google">
                     </a>
