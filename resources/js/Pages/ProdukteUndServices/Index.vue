0979a94920e86d08b9997bf0c29a1bcacaae328a
Morris <maurice.freuwoert@gmail.com>
2023-06-12 11:42:56 +0200

    ~ noch mehr link

--- resources/js/Pages/ProdukteUndServices/Index.vue
+++ resources/js/Pages/ProdukteUndServices/Index.vue
@@ -8,7 +8,7 @@
             <Card name="Foodservice" alt="Foodservice" cover :link="route('ps.foodservice')" image="/images/content/services/cover/foodservice_cover.png"/>
             <Card name="Unsere Marken" alt="Unsere Marken" cover :link="route('ps.marken')" image="/images/content/services/cover/marken_cover.png"/>
             <Card name="Fachberatung: Käse und Salate" alt="Fachberatung: Käse und Salate" cover :link="route('ps.fachberatung-kaese-und-salate')" image="/images/content/services/cover/kaese_und_salate_cover.png"/>
-            <Card name="Marketing & Kommunikation" alt="Marketing & Kommunikation" cover :link="route('ps.marketing-und-kommunikation')" image="/images/content/services/cover/mkbs_cover.png"/>
+            <Card name="Marketing & Kommunikation" alt="Marketing & Kommunikation" cover :link="route('mkbs')" image="/images/content/services/cover/mkbs_cover.png"/>
             <Card name="Technischer Kundendienst" alt="Technischer Kundendienst" cover :link="route('ps.technischer-kundendienst')" image="/images/content/services/cover/technischer_kundendienst_cover.png"/>
             <Card name="Seminare" alt="Seminare" cover :link="route('seminare')" image="/images/content/services/cover/seminare_cover.png"/>
         </div>
