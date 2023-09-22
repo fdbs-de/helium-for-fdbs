78cabf448b113d997b7731172c842934266e43b7
freuwoert <33777960+freuwoert@users.noreply.github.com>
2023-09-22 14:59:13 +0200

    Added link prop to image section

--- resources/js/Pages/Apps/Pages/Sections/ImageSection.vue
+++ resources/js/Pages/Apps/Pages/Sections/ImageSection.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="he-section he-image-section" :style="{padding, backgroundColor}">
         <component class="he-image-section-limiter" :is="LimiterManifest.component" :size="size">
-            <img class="content-image" :style="{height, objectFit}" :src="src" :alt="alt" />
+            <a :href="link" style="display: contents;">
+                <img class="content-image" :style="{height, objectFit}" :src="src" :alt="alt" />
+            </a>
         </component>
     </section>
 </template>
@@ -15,6 +17,7 @@
         size: String,
         src: String,
         alt: String,
+        link: String,
         height: String,
         padding: String,
         backgroundColor: String,
