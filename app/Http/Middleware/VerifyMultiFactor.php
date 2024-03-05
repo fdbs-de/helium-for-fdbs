59c68e517cbddae9f6a9da422d420d6c7097d55b
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-03-05 13:18:17 +0100

    Refactor MFA middleware

--- app/Http/Middleware/VerifyMultiFactor.php
+++ app/Http/Middleware/VerifyMultiFactor.php
@@ -10,17 +10,21 @@ class VerifyMultiFactor
 {
     public function handle(Request $request, Closure $next)
     {
-        // If the user is loggen in via "remember me" cookie, we will just let them through
-        if (Auth::viaRemember()) return $next($request);
+        $mfaEnabled = $request->user()->hasMfaEnabled;
 
-        // At this point, we know that the user is logged in and has enabled MFA
-        // We will check if the user has verified their MFA
-        if ($request->user()->hasMfaEnabled && !session()->has('verified_multi_factor'))
+        // If the user is logging in via "remember me" and has enabled MFA, we will verify their MFA status
+        if ($mfaEnabled && Auth::viaRemember())
+        {
+            session()->put('verified_multi_factor', true);
+        }
+
+        // Now we check if the user has verified their MFA status
+        if ($mfaEnabled && !session()->has('verified_multi_factor'))
         {
             return redirect()->route('mfa');
         }
 
-        // If the user has verified their MFA, we will let them through
+        // Lastly, if the user has verified their MFA status, we can continue
         return $next($request);
     }
 }
