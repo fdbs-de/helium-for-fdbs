826f3cc7ffbb3b3469f8047909724f78a301c635
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-04-18 09:02:46 +0200

    Change Besetzungszeiten

--- resources/js/Pages/Apps/Pages/Sections/Footer.vue
+++ resources/js/Pages/Apps/Pages/Sections/Footer.vue
@@ -37,7 +37,7 @@
                     <br>
                     <b>Bestellhotline:</b><br>
                     0531 - 210 55-23<br>
-                    (besetzt von 06:00 - 24:00)<br>
+                    (besetzt von 07:00 - 16:00)<br>
                 </div>
             </div>
             <div class="block">
